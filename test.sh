e2b1c102029290d84d3d999e184e0747eeec43f6
drodbourne <dale_r_1970@yahoo.com>
2022-05-11 15:15:49 +0000

    DR Updated test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ do
   sudo apt-get install python3 python3-pip python3-venv
   python3 -m venv venv
   source venv/bin/activate
-  pip3 install -r test_requirements.txt
+  pip3 install -r requirements.txt
   python3 -m pytest --cov=application --cov-report=xml --junitxml=junit/test-results.xml
   deactivate
   cd ..
